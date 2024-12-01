2e41a8da8c568a9228f4561d85a14766fdfaefc6
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2024-12-01 19:21:14 +0530

    app.css change

--- Frontend/src/Pages/HeroPage.jsx
+++ Frontend/src/Pages/HeroPage.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Heropage/Navbar";
 import Heading from "../components/Heropage/Heading";
 import Projects from "../components/Projects";
 import ServicesPage from "../components/Services";
-import "../app.css";
+import "../App.css";
 import About from "../components/About";
 import ContactUs from "../components/ContactUs";
 import Footer from "../components/Footer";
